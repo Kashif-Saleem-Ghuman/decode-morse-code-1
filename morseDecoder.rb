a69296acde1723a3b79bf99977a546c69b5286ce
rtonata88 <rtonata@gmail.com>
2022-03-02 22:37:07 +0200

    Add word decoder method

--- morseDecoder.rb
+++ morseDecoder.rb
@@ -31,3 +31,13 @@ def get_letter(letter)
     @current_letter = $dictionary[letter]
     return @current_letter
 end
+
+def decode_word(word)
+    @words = word.split
+    @current_word = ''
+    @words.each do |letter| get_letter(letter)
+        @current_word += get_letter(letter)
+    end
+    print @current_word + ' '
+    return @current_word
+end
